6c215c6587267b9be4803801ae006e4df34b9a6a
Tyler May <tylermay@Tylers-MacBook-Air.local>
2023-09-26 08:10:17 -0600

    text validation app black diamond changes

--- textValidationApp/ViewController.swift
+++ textValidationApp/ViewController.swift
@@ -8,20 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var label: UILabel!
     
     @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    let password = "ILOVE2CODE!"
+    let password = "ILove2Code!"
     var mylabel = "Please login"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        loginButton.layer.cornerRadius = 10
     }
-
-
+    
+    
     @IBAction func loginButtonTapped(_ sender: Any) {
         guard let user = usernameTextField.text else {
             mylabel = "Something went wrong, please try again."
@@ -36,24 +38,31 @@ class ViewController: UIViewController {
             return
         }
         guard !user.isEmpty else {
-            mylabel = "Something went wrong, please try again."
+            mylabel = "Username is required."
             label.textColor = .red
             label.text = mylabel
             return
         }
-        guard pass == password else {
-            mylabel = "Something went wrong, please try again."
+        if isValidEmail(email: user) {
+            if pass == password {
+                mylabel = "Login Successful"
+                label.textColor = .green
+                label.text = mylabel
+            } else {
+                mylabel = "Incorrect password. Please try again."
+                label.textColor = .red
+                label.text = mylabel
+            }
+        } else {
+            mylabel = "Invalid email address. Please enter a valid email."
             label.textColor = .red
             label.text = mylabel
-            return
         }
-        
-        mylabel = "Login Succesfull"
-        label.textColor = .green
-        label.text = mylabel
-        
     }
     
-    
+    func isValidEmail(email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
 }
-
